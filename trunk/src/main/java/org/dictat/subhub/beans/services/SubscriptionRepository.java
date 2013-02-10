ef28e95b397bed7737ddceb4965e167422c31329
laszlo.hornyak@gmail.com <laszlo.hornyak@gmail.com@e517c4b7-e254-fe86-c9d4-ff66d64a6623>
2013-02-10 17:20:52 +0000

    added method to find expiring subscriptions

--- trunk/src/main/java/org/dictat/subhub/beans/services/SubscriptionRepository.java
+++ trunk/src/main/java/org/dictat/subhub/beans/services/SubscriptionRepository.java
@@ -1,8 +1,12 @@
 package org.dictat.subhub.beans.services;
 
+import java.util.Date;
+import java.util.List;
+
 import org.dictat.subhub.beans.Subscription;
 
 public interface SubscriptionRepository {
 	Subscription getByUrl(String url);
 	void save(Subscription subscription);
+	List<Subscription> findExpring(Date time);
 }
--- trunk/src/main/java/org/dictat/subhub/beans/services/mongo/MongoSubscriptionRepository.java
+++ trunk/src/main/java/org/dictat/subhub/beans/services/mongo/MongoSubscriptionRepository.java
@@ -1,7 +1,10 @@
 package org.dictat.subhub.beans.services.mongo;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.dictat.subhub.beans.Subscription;
 import org.dictat.subhub.beans.services.SubscriptionRepository;
 import org.slf4j.Logger;
@@ -10,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 
@@ -42,14 +46,24 @@ public class MongoSubscriptionRepository implements SubscriptionRepository {
 		if (res == null) {
 			return null;
 		}
+		final Subscription subscription = dbObjToSubscription(res);
+		return subscription;
+	}
+
+	private Subscription dbObjToSubscription(DBObject res) {
 		final Subscription subscription = new Subscription();
-		subscription.setHub(res.get("h").toString());
-		subscription.setUrl(res.get("u").toString());
+		subscription.setId(stringOrNull(res, "_id"));
+		subscription.setHub(stringOrNull(res, "h"));
+		subscription.setUrl(stringOrNull(res, "u"));
 		subscription.setLease((Integer) (res.get("l")));
 		subscription.setLastResubscribe((Date) res.get("lr"));
 		subscription.setSubscribed((Date) res.get("s"));
-		subscription.setVerifyToken((String) res.get("v"));
+		subscription.setVerifyToken(stringOrNull(res, "v"));
 		return subscription;
+	}
+
+	private String stringOrNull(DBObject res, String key) {
+		return res.get(key) == null ? null : res.get(key).toString();
 	}
 
 	private DB getDataBase() {
@@ -72,6 +86,9 @@ public class MongoSubscriptionRepository implements SubscriptionRepository {
 				.append("lr", subscription.getLastResubscribe())
 				.append("l", subscription.getLease())
 				.append("u", subscription.getUrl());
+		if (subscription.getId() != null) {
+			obj.append("_id", new ObjectId(subscription.getId()));
+		}
 		logger.debug("subscription saved to repo: {}", subscription.getUrl());
 		getColl().save(obj);
 	}
@@ -82,6 +99,29 @@ public class MongoSubscriptionRepository implements SubscriptionRepository {
 
 	public void setCollection(String collection) {
 		this.collection = collection;
+	}
+
+	public List<Subscription> findExpring(Date time) {
+		ArrayList<Subscription> subscriptions = new ArrayList<Subscription>();
+		BasicDBObject query = new BasicDBObject();
+		BasicDBObject condition = new BasicDBObject();
+		condition.append("$lt", time);
+		query.append("lr", condition);
+		addSubs(subscriptions, query);
+
+		BasicDBObject nullQuery = new BasicDBObject();
+		nullQuery.append("lr", null);
+		addSubs(subscriptions, nullQuery);
+
+		return subscriptions;
+	}
+
+	private void addSubs(List<Subscription> subscriptions, BasicDBObject query) {
+		try (DBCursor cursor = getColl().find(query)) {
+			while (cursor.hasNext()) {
+				subscriptions.add(dbObjToSubscription(cursor.next()));
+			}
+		}
 	}
 
 }
