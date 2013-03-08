9d1f752345f6c7e96dad322e49f7a9fd74cb56ce
laszlo.hornyak@gmail.com <laszlo.hornyak@gmail.com@e517c4b7-e254-fe86-c9d4-ff66d64a6623>
2013-03-08 22:40:07 +0000

    new Poll Schedulre component

---
+++ src/test/java/org/dictat/subhub/beans/services/poll/PollSchedulerTest.java
@@ -0,0 +1,20 @@
+package org.dictat.subhub.beans.services.poll;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.todomap.feed.HttpClientReader;
+
+public class PollSchedulerTest {
+	@Test
+	public void testGetPollFrequency() throws IOException {
+		long pollFrequency = new PollScheduler().getPollFrequency(HttpClientReader.read("http://blog.dictat.org/?feed=rss2"));
+		System.out.println(pollFrequency / (60000) + " minutes");
+		pollFrequency = new PollScheduler().getPollFrequency(HttpClientReader.read("http://tifyty.wordpress.com/feed/"));
+		System.out.println(pollFrequency / (60000) + " minutes");
+		pollFrequency = new PollScheduler().getPollFrequency(HttpClientReader.read("http://index.hu/24ora/rss/"));
+		System.out.println(pollFrequency / (1000) + " seconds");
+		pollFrequency = new PollScheduler().getPollFrequency(HttpClientReader.read("http://origo.hu/contentpartner/rss/hircentrum/origo.xml"));
+		System.out.println(pollFrequency / (60000) + " minutes");
+	}
+}
